7ad71a8d5dac52e7676c5a58c7d75d81ebffd26e
hrhr49 <hiro49410@gmail.com>
2020-06-28 12:10:51 +0900

    fix peek.sh

--- scripts/cli/install/peek.sh
+++ scripts/cli/install/peek.sh
@@ -2,6 +2,10 @@
 
 # Gifで録画できるやつ
 
-sudo add-apt-repository ppa:peek-developers/stable
-sudo apt update
-sudo apt install peek
+if type "peek" > /dev/null 2>&1; then
+    echo "peek is already installed"
+else
+    sudo add-apt-repository ppa:peek-developers/stable
+    sudo apt update
+    sudo apt install peek
+fi
