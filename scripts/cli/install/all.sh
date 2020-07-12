85bae87294c0f50929974407447bad3d74d4cd59
hrhr49 <hiro49410@gmail.com>
2020-07-12 16:22:24 +0900

    fix script.

--- scripts/cli/install/all.sh
+++ scripts/cli/install/all.sh
@@ -3,7 +3,7 @@
 # 各種ツールなどをインストールするスクリプト
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-find "$SCRIPT_DIR" -maxdepth 1 | while read -r scriptfile ; do
+find "$SCRIPT_DIR" -maxdepth 1 -type f | while read -r scriptfile ; do
     # 実行可能なら
     if [ -x "$scriptfile" ]; then
         # このファイルじゃなければ
