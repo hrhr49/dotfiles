c84ee2020c5d0b40ff8ad11aea6785fa89862549
hrhr49 <hiro49410@gmail.com>
2020-07-28 19:23:44 +0900

    update nimble.sh

--- scripts/cli/update/nimble.sh
+++ scripts/cli/update/nimble.sh
@@ -5,6 +5,18 @@ packages=(
     # クロスプラットフォームGUIライブラリ
     # nimxの方はWindowsだと別途SDL2を用意する必要がありそう？
     nigui
+
+    # マルチバイト文字を含む文字列の表示幅を取得
+    eastasianwidth
+
+    # マルチバイト文字を含む文字列を整列
+    alignment
+
+    # 関数から自動でCLIを作成
+    cligen
+
+    # REPL
+    inim
 )
 
 if type "nimble" > /dev/null 2>&1; then
