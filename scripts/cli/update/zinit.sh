ff7d2d192fe131b7f1fe1b79c599d42ec88edf60
hrhr49 <hiro49410@gmail.com>
2020-09-28 23:00:53 +0900

    fix zinit update script.

--- scripts/cli/update/zinit.sh
+++ scripts/cli/update/zinit.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 if type "zsh" > /dev/null 2>&1; then
-  # 一旦、zshをインタラクティブモードで起動することで、zinitのパッケージインストール処理を実施する。
-  zsh -i -c exit
+  if [ -f ~/.zinit/bin/zinit.zsh ]; then
+    source ~/.zinit/bin/zinit.zsh
+  fi
 fi
 
