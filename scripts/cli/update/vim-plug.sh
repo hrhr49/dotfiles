bc3b6559e760d764aa6c4e7887f98ba652b3badd
hrhr49 <hiro49410@gmail.com>
2020-11-21 11:26:53 +0900

    update vim-plug config.

--- scripts/cli/update/vim-plug.sh
+++ scripts/cli/update/vim-plug.sh
@@ -2,13 +2,13 @@
 set -e
 
 if type "vim" > /dev/null 2>&1; then
+  # 「警告: 端末からの入力ではありません」を暫定的に回避する
   # vim-plug
-  vim -c 'PlugInstall|qa'
+  echo dummy | vim -c 'PlugInstall|qa!' -
 
-  # 以下を行うと、端末の入力がおかしくなるので一旦保留
   # coc.nvimの更新
   # 先にディレクトリ作っておかないと、Vim側でディレクトリ作成時にENTERキー入力待ちになってしまう。
-  # mkdir -p ~/.config/coc
+  mkdir -p ~/.config/coc
   # vimrcのg:coc_global_extensionsのを全部Installする。dockerビルド時だと、何も表示されない時間が数分続いたけど、ちゃんとできていた。
-  # vim -c 'execute "CocInstall -sync " . join(g:coc_global_extensions, " ")|qa'
+  echo dummy | vim -c 'execute "CocInstall -sync " . join(g:coc_global_extensions, " ")|qa!' -
 fi
