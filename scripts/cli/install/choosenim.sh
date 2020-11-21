cdbc91a847896fdf8455852ccdb6811aee7aff1c
hrhr49 <hiro49410@gmail.com>
2020-11-21 21:07:11 +0900

    update choosenim installation script.

--- scripts/cli/install/choosenim.sh
+++ scripts/cli/install/choosenim.sh
@@ -4,5 +4,9 @@ set -e
 if type choosenim > /dev/null 2>&1; then
   echo 'choosenim is already installed'
 else
-  curl 'https://nim-lang.org/choosenim/init.sh' -sSf | sh
+  if type "yay" > /dev/null 2>&1; then
+    echo 'yay found. please install by yay.'
+  else
+    curl 'https://nim-lang.org/choosenim/init.sh' -sSf | sh
+  fi
 fi
