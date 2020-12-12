c60d1344f4642721cd01183945c36a14bdff288e
hrhr49 <hiro49410@gmail.com>
2020-12-12 16:07:38 +0900

    add pkg config for termux.

---
+++ scripts/cli/update/pkg.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+# pkgでインストールするものリスト(termux用)
+packages=(
+  termux-api
+  openssh
+
+  bat
+  ctags
+  curl
+  fd
+  fzf
+  git
+  htop
+  hub
+  jq
+  make
+  man
+  neofetch
+  neovim
+  nodejs
+  perl
+  python
+  ranger
+  lf
+  ripgrep
+  tig
+  tmux
+  tokei
+  tree
+  unzip
+  vim
+  wget
+  zoxide
+  zsh
+)
+
+
+if type "pkg" > /dev/null 2>&1; then
+  pkg install -y "${packages[@]}"
+fi
